5f7bcdbe22c0ebdcfc1a55a46435fdbba4ac078b
Akshay Shekhervoldyman666@gmail.com <voldyman666@gmail.com>
2015-04-15 15:25:03 +0530

    Improved implementation

--- server.go
+++ server.go
@@ -1,32 +1,62 @@
 package main
 
-import(  "fmt"
-         "github.com/gorilla/mux"
-         "net/http"
-         "github.com/kidoman/embd"
-       _ "github.com/kidoman/embd/host/rpi" // This loads the RPi driver
-         "log"
-      )
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-var light = 1
-func Toggle(w http.ResponseWriter, r *http.Request) {
-    if light == 1 {
-        light = 0
-        embd.DigitalWrite(10, embd.High)
-        fmt.Fprintf(w, "OFF")
-    } else {
-        light = 1
-        embd.DigitalWrite(10, embd.Low)
-        fmt.Fprintf(w, "ON")
-    }
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/rpi" // This loads the RPi driver
+)
+
+type WebController struct {
+	pin   int
+	pinOn bool
+}
+
+func newWebController(pinNumber int) *WebController {
+	w := new(WebController)
+	w.pin = pinNumber
+	w.pinOn = false
+
+	return w
+}
+
+func (w *WebController) Toggle() (curState bool) {
+	if w.pinOn {
+		embd.DigitalWrite(w.pin, embd.High)
+	} else {
+		embd.DigitalWrite(w.pin, embd.Low)
+	}
+
+	w.pinOn = !w.pinOn
+
+	return w.pinOn
+}
+
+func (wc *WebController) Handle(w http.ResponseWriter, r *http.Request) {
+	pinOn := wc.Toggle()
+
+	output := "Device is switched %s"
+	if pinOn {
+		fmt.Fprintf(w, output, "on")
+	} else {
+		fmt.Fprintf(w, output, "off")
+	}
+}
+
+func startHttp() {
+	// start web controller on pin 10
+	wc := newWebController(10)
+
+	http.HandleFunc("/", wc.Handle)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func main() {
-    embd.InitGPIO()
-    defer embd.CloseGPIO()
-    embd.SetDirection(10, embd.Out)
-    r := mux.NewRouter()
-    r.HandleFunc("/", Toggle)
-    http.Handle("/", r)
-    log.Fatal(http.ListenAndServe(":8", r))
+	embd.InitGPIO()
+	defer embd.CloseGPIO()
+	embd.SetDirection(10, embd.Out)
+
+	startHttp()
 }
